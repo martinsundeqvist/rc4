80ed5e1e756e4cdf5373317a2a2373fd67102536
Martin Sundeqvist <martin.sundequist@gmail.com>
2023-12-24 01:09:12 +0100

    Added unit test based on reference implementation output

--- src/test/java/com/martinsundeqvist/streamcipher/AppTest.java
+++ src/test/java/com/martinsundeqvist/streamcipher/AppTest.java
@@ -11,8 +11,24 @@ import com.martinsundeqvist.streamcipher.App.Rc4Cipher;
 public class AppTest 
 {
     @Test
-    public void alwaysTrue()
+    public void HELLOWORLD_encodes_correctly()
     {
-        assertTrue(true);
+        int[] key = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
+        byte[] plaintextBytes = "HELLOWORLD".getBytes();
+        Rc4Cipher cipher = new Rc4Cipher(key, key.length);
+
+        cipher.ksa();
+
+        int[] s = cipher.ksa();
+
+        int[] keystream = cipher.prng(s, plaintextBytes.length);
+
+        int[] encryptedData = cipher.encrypt(plaintextBytes, keystream);
+        int[] expectedData = new int[] {223, 238, 198, 87, 191, 248, 246, 51, 126, 182};
+
+        assertEquals(encryptedData.length, expectedData.length);
+        for (int i = 0; i < encryptedData.length; i++)
+            assertEquals(encryptedData[i], expectedData[i]);
+
     }
 }
